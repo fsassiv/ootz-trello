577b5071180ab48fb8f26dc273883423a787ede0
Flavio Andrade <flavio.andrade@agilize.com.br>
2020-08-02 22:09:18 -0300

    remove apikey

--- service/api.js
+++ service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://api.todo.ootz.com.br",
   headers: {
-    apiKey: "38476616AB64ACDCAC9D2835A50C2B7074DE956BA107B7F7885D64E1F26DFDE1"
+    apiKey: ""
   }
 });
 
